e29fdc271c8782770918833b8350252e20c11e1f
ahsansabir30 <ahsansabir30@gmail.com>
2022-08-30 15:00:15 +0000

    adding docker-compose

--- s1/application/routes.py
+++ s1/application/routes.py
@@ -4,11 +4,11 @@ import requests
 
 @app.route('/football', methods=['POST', 'GET'])
 def football():
-    team = requests.get('http://localhost:5001/get/teams').text
-    stadium = requests.get('http://localhost:5002/get/stadiums').text
+    team = requests.get('http://s2:5001/get/teams').text
+    stadium = requests.get('http://s3:5002/get/stadiums').text
     response = {
         'team': team,
         'stadium': stadium
     }
-    outcome = requests.post('http://localhost:5003/outcome', json=response).text
+    outcome = requests.post('http://s4:5003/outcome', json=response).text
     return render_template('generator.html', team=team, stadium=stadium, outcome=outcome)
