4b7854f79516edea9e524fc9cde0af7c6e6524a3
ahsansabir30 <ahsansabir30@gmail.com>
2022-08-31 12:41:02 +0000

    made changes to jenkin file, pull from branch ansible

--- test-script.sh
+++ test-script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ -d SFIA2 ]; then
-    cd SFIA2 && git pull origin dev
+    cd SFIA2 && git pull origin ansible
 else
-    git clone --single-branch --branch dev https://github.com/ahsansabir30/SFIA2.git SFIA2
+    git clone --single-branch --branch ansible https://github.com/ahsansabir30/SFIA2.git SFIA2
     cd SFIA2
 fi
 
