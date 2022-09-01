66b6c3db538c20920a782c77368aa0edca2c4142
ahsansabir30 <ahsansabir30@gmail.com>
2022-09-01 18:51:47 +0000

    editing create-table script

--- create-table.sh
+++ create-table.sh
@@ -1,5 +1,5 @@
 while getopts "c" opt; do
     case ${opt} in 
-        c) echo -e "docker exec -it s1 python create.py";;
+        c) echo -e "cd project && cd s1 && docker exec -it s1 python create.py";;
     esac
-done
+done    
