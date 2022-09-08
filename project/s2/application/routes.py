819b69e3dc10000ea6082eb291e34c4aa4f6dc21
ahsansabir30 <ahsansabir30@gmail.com>
2022-09-08 13:07:32 +0000

    ci test - demo

--- project/s2/application/routes.py
+++ project/s2/application/routes.py
@@ -5,7 +5,7 @@ import random
 @app.route('/get/teams', methods=['GET'])
 def getteams():
     teams = [
-        'Liverpool', 'Chelsea', 'Manchester United', 'Manchester City', 'Tottenham', 'Arsenal'
+        'Bayern Munich', 'Borussia Dortmund', 'VfL Wolfsburg', 'Leverkusen', 'Eintracht Frankfurt', 'Borussia Mönchengladbach'
     ]
     random_team = random.choice(teams)
     return Response(random_team, mimetype='text/plain') 
